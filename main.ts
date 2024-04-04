f909fc5a21f8d68f00eebadb9f2a0b4b3896add4
AI-CodeFusion <syed19.freelancer786@gmail.com>
2024-04-04 19:47:23 +0500

    did name cases

---
+++ main.ts
@@ -0,0 +1,7 @@
+// Task 03:- 
+
+//Stored a name in a variable.
+let personName = "Syeda Binish";
+console.log("LowerCase:",personName.toLowerCase());      // print in lowecase
+console.log("UpperCase:",personName.toUpperCase());      // print in uppercase
+console .log("TitleCase:",personName.replace(/\bw/g,c => c.toUpperCase()));  //print in titlecase
